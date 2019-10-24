e4f2833740a1d6dc76a7951714fd80c3dbbdbf67
bearchit <bearchit@gmail.com>
2019-10-25 08:02:28 +0900

    problems with Tabs

--- lib/profile/profile_showcase.dart
+++ lib/profile/profile_showcase.dart
@@ -15,22 +15,61 @@ class _ProfileShowcaseState extends State<ProfileShowcase>
   }
 
   Widget buildPosts(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 3,
-      children: List.generate(
-        50,
-        (index) => Padding(
-          padding: const EdgeInsets.all(0.5),
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                fit: BoxFit.cover,
-                image:
-                    NetworkImage("https://picsum.photos/200?random&t=$index"),
-              ),
-            ),
+    return DefaultTabController(
+      length: 2,
+      child: Row(
+        children: <Widget>[
+          TabBar(
+            tabs: <Widget>[
+              Icon(Icons.grid_on),
+              Icon(Icons.account_box),
+            ],
           ),
-        ),
+          TabBarView(
+            children: <Widget>[
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  children: List.generate(
+                    50,
+                    (index) => Padding(
+                      padding: const EdgeInsets.all(0.5),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                                "https://picsum.photos/200?random&t=$index"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 3,
+                  children: List.generate(
+                    50,
+                    (index) => Padding(
+                      padding: const EdgeInsets.all(0.5),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                                "https://picsum.photos/200?random&t=$index"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
